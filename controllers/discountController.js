3e451993ce2547a3a9a1fbded9ca1e28a743a3a3
Himanshu Raj <himanshuraj6771@gmail.com>
2024-10-09 21:20:58 +0530

    Now every offer get included with stores details

--- controllers/discountController.js
+++ controllers/discountController.js
@@ -11,54 +11,57 @@ const discountController = async (req, res, next) => {
     // longitude and latitude of end user
     // any other which apply
     try {
-        const { category, search, type, rating, distance, discount, longitude, latitude } = req.body;
+        const { category, search, type, rating, distance, discount, longitude, latitude, page = 1, limit = 10 } = req.body;
         
+        // Calculating offset
+        const offset = (page - 1) * limit; 
+
         // Initial query for stores
-        let storeQuery = {
-            where: {
-                status: 'active'
-            },
-            attributes: ['store_name', 'address', 'latitude', 'longitude', 'category', 'rating', 'opening_hours'],
+        let offerQuery = {
+            where: {},
+            attributes: ['offer_id', 'offer_type', 'description', 'discount_percentage', 'minimum_purchase', 'maximum_discount', 'end_date', 'terms_conditions', 'featured'],
             include: [{
-                model: Offers,
-                where: {},
-                attributes: ['offer_id', 'offer_type', 'description', 'discount_percentage', 'minimum_purchase', 'maximum_discount', 'end_date', 'terms_conditions', 'featured']
-            }]
+                model: Store,
+                where: {status: 'active'},
+                attributes: ['store_name', 'address', 'latitude', 'longitude', 'category', 'rating', 'opening_hours'],
+            }],
+            limit,
+            offset
         }
 
         // Checking for partcular category
         if (category) {
-            storeQuery.where.category = category;
+            offerQuery.include[0].where.category = category;
         }
 
         // Checking for particular offer type
         if (type) {
-            storeQuery.include[0].where.offer_type = type;
+            offerQuery.where.offer_type = type;
         }
 
         // Checking for rating which is greater than or equal to
         if (rating) {
-            storeQuery.where.rating = {[Op.gte]: parseFloat(rating)}
+            offerQuery.include[0].where.rating = {[Op.gte]: parseFloat(rating)}
         }
 
         // Checking for discount which is greater than or equal to
         if (discount) {
-            storeQuery.include[0].where.discount_percentage = {[Op.gte]: parseFloat(discount)}
+            offerQuery.where.discount_percentage = {[Op.gte]: parseFloat(discount)}
         }
 
         // Checking for particular search by the user
         if (search) {
-            storeQuery.where[Op.or]= [  
-                {store_name: {[Op.like]: `%${search}%`}},
-                {category: {[Op.like]: `%${search}%`}},
-                {address: {[Op.like]: `%${search}%`}},
-                { '$offers.offer_type$': { [Op.like]: `%${search}%` } },
-                { '$offers.description$': { [Op.like]: `%${search}%` } },
-                { '$offers.discount_percentage$': { [Op.like]: `%${search}%` } }
+            offerQuery.where[Op.or]= [  
+                {'$Store.store_name$': {[Op.like]: `%${search}%`}},
+                {'$Store.category$': {[Op.like]: `%${search}%`}},
+                {'$Store.address$': {[Op.like]: `%${search}%`}},
+                { offer_type: { [Op.like]: `%${search}%` } },
+                { description: { [Op.like]: `%${search}%` } },
+                { discount_percentage: { [Op.like]: `%${search}%` } }
             ];
         }
 
-        const stores = await Store.findAll(storeQuery);
+        const {count, rows} = await Offers.findAndCountAll(offerQuery);
 
         // Adding distance field in each store to show the store
         // const storesWithoffersAndDistance = stores.map(store => {  
@@ -67,7 +70,19 @@ const discountController = async (req, res, next) => {
         //         ...store.get()
         //     }
         // })
-        return res.status(200).json({success: true, message: stores});
+
+        // Calculating the number of pages
+        const totalPages = Math.ceil(count / limit);
+        return res.status(200).json({
+            success: true,
+            data: rows,
+            pagination: {
+                totalItems: count,
+                totalPages: totalPages,
+                currentPage: page,
+                limit: limit
+            }
+        });
         
     }
     catch (error) {
