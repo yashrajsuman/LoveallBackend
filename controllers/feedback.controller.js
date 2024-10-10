12fbe3bde03048bfd916b7fe08bff49ff01a4b51
Himanshu Raj <himanshuraj6771@gmail.com>
2024-10-11 00:31:00 +0530

    Feedback controller has been completed

---
+++ controllers/feedback.controller.js
@@ -0,0 +1,42 @@
+import Feedback from "../models/feedback.model.js";
+import {Offers} from "../models/association.js";
+import { where } from "sequelize";
+
+
+const feedback = async (req, res, next) => {
+    const {store_id, offer_id, comments, rating} = req.body;
+
+    // Vaalidate rating
+    if (!store_id || !offer_id || !rating) {
+        return res.status(201).json({success: false, message: "All fields are Compulsory."});
+    }
+
+    // Ensure rating should be between 1 and 5
+    if (rating < 1 || rating > 5) {
+        return res.status(400).json({success: false, message: "Rating must be between 0 and 5"});
+    }
+    try {
+
+        // Check offer exist or not
+        const offer = await Offers.findOne({
+            where: {store_id, offer_id}
+        });
+        if (!offer) {
+            return res.status(404).json({success: false, message: "Offer doesn't exist."});
+        }
+
+        // Create the feedback
+        const feedback = await Feedback.create({user_id: req.user.id, store_id, offer_id, comments, rating})
+        if (feedback) {
+            return res.status(201).json({success: true, message: "Thank you for giving feedback", data: feedback});
+        }
+        else {
+            return res.status(500).json({success: false, message: "Something error occured"});
+        }
+    }
+    catch (error) {
+       return next(error); 
+    }
+}
+
+export default feedback;
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 import discountController from "../controllers/discountController.js";
 import transaction from "../controllers/transaction.controller.js";
 import { authMiddleware } from "../middleware/isAuthenticated.js";
+import feedback from "../controllers/feedback.controller.js";
 
 const router = Router();
 
 router.get('/discount', discountController);
 router.get('/dashboard',authMiddleware, transaction);
+router.post('/feedback', authMiddleware, feedback)
 
 export default router;
